d06244d205460f320dd52be8e7766619658a5302
hankai <john.han.kai@gmail.com>
2016-06-27 10:49:42 +0800

    删除多余注释

--- src/main/java/ren/hankai/config/Route.java
+++ src/main/java/ren/hankai/config/Route.java
@@ -10,28 +10,10 @@ package ren.hankai.config;
  */
 public final class Route {
 
-    /**
-     * Web service URL前缀
-     */
     private static final String API_PREFIX        = "/api";
-    /**
-     * ios manifest 文件生成
-     */
     public static final String  API_LOGIN         = API_PREFIX + "/login";
-    /**
-     * 网站后台URL前缀
-     */
     public static final String  BACKGROUND_PREFIX = "/admin";
-    /**
-     * 后台登录
-     */
     public static final String  BG_LOGIN          = BACKGROUND_PREFIX + "/login";
-    /**
-     * 后台应用列表
-     */
     public static final String  BG_SAMPLE         = BACKGROUND_PREFIX + "/sample";
-    /**
-     * 网站前台应用列表
-     */
     public static final String  FG_SAMPLE         = "/sample";
 }
