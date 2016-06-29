c0a2728e1c680aafb87840c885dd97f9d47f596d
hankai <john.han.kai@gmail.com>
2016-06-29 09:54:05 +0800

    修复错误类型引用可能导致的命令行编译失败

--- setenv.sh
+++ setenv.sh
@@ -3,4 +3,4 @@ TCP_PORT=8000
 
 # More arguments passed to spring boot or jvm
 # EXTRA_ARGS="--proxy.name=www.example.org --proxy.port=80 --proxy.scheme=http"
-EXTRA_ARGS=--proxy.name=apps.sparksoft.com.cn --proxy.port=443
+EXTRA_ARGS=
