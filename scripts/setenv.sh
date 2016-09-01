f57b222196291f7ee22eb37166a143a5f2c828c9
hankai <john_han@163.com>
2016-09-01 21:54:54 +0800

    环境变量脚本补充TCP端口参数

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,4 +1,10 @@
+#! /bin/sh
+#
+# Set environment variables
+#
+
 TCP_LISTEN=0.0.0.0
+TCP_PORT=8000
 
 # specify absolute path of "java" command here or keep default to use java that configured
 # in environment variables
